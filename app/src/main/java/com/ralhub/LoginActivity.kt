f8e3e52750dbbe597053afe56c8f5971868f5299
Sangwan <jklom0326@gmail.com>
2020-08-18 02:54:02 +0900

    Initial commit

---
+++ app/src/main/java/com/ralhub/LoginActivity.kt
@@ -0,0 +1,85 @@
+package com.ralhub
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.CheckedTextView
+import android.widget.Toast
+import com.facebook.*
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import java.util.*
+
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var auth : FirebaseAuth
+    lateinit var callbackManger : CallbackManager
+    lateinit var checkedTextView : CheckedTextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        auth = FirebaseAuth.getInstance()
+
+        callbackManger = CallbackManager.Factory.create()
+        checkedTextView = findViewById(R.id.checkedTextView)
+        checkedTextView.setOnClickListener {
+            facebookLogin()
+        }
+    }
+    fun facebookLogin(){
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile","email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManger, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    handleFacebookAccessToken(loginResult.accessToken)
+                }
+
+                override fun onCancel() {
+                }
+
+                override fun onError(error: FacebookException?) {
+                    Log.e("Callback :: ", "onError : " + error?.stackTrace);
+                }
+
+            })
+    }
+
+    fun handleFacebookAccessToken(token : AccessToken){
+        val credential = FacebookAuthProvider.getCredential(token.token!!)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener {
+                    task ->
+                if (task.isSuccessful){
+                    moveMainPage(task.result?.user)
+                }else{
+                    Toast.makeText(this,task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManger.onActivityResult(requestCode,resultCode,data)
+    }
+
+    fun moveMainPage(user: FirebaseUser?){
+        if (user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        finish()
+    }
+}
---
+++ app/src/main/java/com/ralhub/LoginMainActivity.kt
@@ -0,0 +1,40 @@
+package com.ralhub
+
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+
+class LoginMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_main)
+
+        lateinit var Button1: Button // 회원가입 버튼
+        lateinit var Button2: Button // 로그인 버튼
+
+        Button1 = findViewById(R.id.button1)
+        Button2 = findViewById(R.id.button2)
+
+        Button1.setOnClickListener {
+            val intent = Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        Button2.setOnClickListener {
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        finish()
+    }
+}
